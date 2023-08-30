346a9e9ecfea93e903d19a1cd61d15b626f7cd9b
Nicole Lai <nicole.lai@evergreensystemltd.com>
2023-08-30 14:31:15 +0800

    improve same tree algo so it runs faster

--- 100.same-tree.py
+++ 100.same-tree.py
@@ -14,28 +14,13 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-        elif (p and not q) or (not p and q):
-            return False
-        if p.val != q.val:
-            return False
-
-        if p.left and q.left:
-            left = self.isSameTree(p.left, q.left)
-        elif not p.left and not q.left:
-            left = True
-        else:
-            left = False
-
-        if p.right and q.right:
-            right = self.isSameTree(p.right, q.right)
-        elif not p.right and not q.right:
-            right = True
-        else:
-            right = False
-
-        return all([left, right])
+        if p and q:
+            return (
+                p.val == q.val
+                and self.isSameTree(p.left, q.left)
+                and self.isSameTree(p.right, q.right)
+            )
+        return p == q
 
 
 # @lc code=end
