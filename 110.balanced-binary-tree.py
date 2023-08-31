366dac280c85ded5518cec8607bca014c4a9f510
Nicole Lai <nicole.lai@evergreensystemltd.com>
2023-08-31 13:50:59 +0800

    calculate height

---
+++ 110.balanced-binary-tree.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=110 lang=python3
+#
+# [110] Balanced Binary Tree
+#
+
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        return (
+            abs(self.height(root.left) - self.height(root.right)) < 2
+            and self.isBalanced(root.left)
+            and self.isBalanced(root.right)
+        )
+
+    def height(self, root):
+        if not root:
+            return 0
+        return max(self.height(root.left), self.height(root.right)) + 1
+
+
+# @lc code=end
