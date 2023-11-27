fe0a74254d8fa875769ac885524ca92293f99750
yuiasora1024 <tcnlai@connect.ust.hk>
2023-11-27 13:52:01 -0500

    a bit faster

--- 392.is-subsequence.py
+++ 392.is-subsequence.py
@@ -10,20 +10,20 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         if not s:
             return True
-        if not t:
+        if len(s) > len(t):
             return False
+
         ps, pt = 0, 0
         while ps < len(s) and pt < len(t) and len(t) >= len(s):
+            if t.startswith(s):
+                return True
             if s[ps] == t[pt]:
                 ps += 1
                 pt += 1
             else:
                 t = t[:pt] + t[pt + 1 :]
 
-        if t.startswith(s):
-            return True
-        else:
-            return False
+        return False
 
 
 # @lc code=end
