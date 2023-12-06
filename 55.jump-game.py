fc7a79d7b421dab59f3f7764e0fe4f80577340c0
yuiasora1024 <tcnlai@connect.ust.hk>
2023-12-06 15:31:17 -0500

    after forum

--- 55.jump-game.py
+++ 55.jump-game.py
@@ -8,22 +8,12 @@
 # @lc code=start
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
-        if nums[0] == 0:
-            return False
-        arr = [True] + [False] * (len(nums) - 1)
-        # exhaust all the possibilities
-        for n in range(len(nums)):
-            if arr[n]:
-                for i in range(n, n + nums[n] + 1):
-                    if i < len(nums):
-                        arr[i] = True
-            if arr[-1]:
-                return True
-        print(arr)
-
-        return False
+        p = 0
+        for i in range(len(nums)):
+            if i > p:
+                return False
+            p = max(p, i + nums[i])
+        return True
 
 
 # @lc code=end
