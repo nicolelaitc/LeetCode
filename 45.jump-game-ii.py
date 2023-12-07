3fe576cc3de39dd9603b88bfd0288e3e1b4fb7f1
yuiasora1024 <tcnlai@connect.ust.hk>
2023-12-07 13:39:50 -0500

    after reading forum; great way to store two pointers

--- 45.jump-game-ii.py
+++ 45.jump-game-ii.py
@@ -8,30 +8,17 @@
 # @lc code=start
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        p = 0
-        count = 0
-        while len(nums) > 1:
-            if nums[0] >= len(nums) - 1:
+        # we check the distance that can reach from curr_start, and then comparing every node within the distance that who can go longest, and then next time start from that
+        curr_longest, curr_end, count = 0, 0, 0
+        for i in range(len(nums) - 1):
+            if i + nums[i] >= len(nums) - 1:
                 return count + 1
-            arr = [n + i for i, n in enumerate(nums[1 : nums[0] + 1])]
-            maxVal = max(arr)
-            print(f"arr={arr}, maxVal={maxVal}")
-            p = arr.index(maxVal) + 1
-            count += 1
-            nums = nums[p:]
-        return count
+            curr_longest = max(curr_longest, i + nums[i])
+            if i == curr_end:
+                curr_end = curr_longest
+                count += 1
 
-        # while p < len(nums) - 1:
-        #     if (p + nums[p]) >= len(nums) - 1:
-        #         return count + 1
-        #     arr = nums[p + 1 : p + nums[p] + 1]
-        #     print(arr)
-        #     maxVal = max(arr)
-        #     p = nums.index(maxVal)
-        #     count += 1
-        #     print(f"p: {p}")
-        #     nums = nums[p:]
-        # return count
+        return count
 
 
 # @lc code=end
