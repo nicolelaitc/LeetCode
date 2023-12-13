5426a1c11098875df925658f3e77e48dcdc9076b
yuiasora1024 <tcnlai@connect.ust.hk>
2023-12-13 13:48:58 -0500

    try speed up with early return

--- 209.minimum-size-subarray-sum.py
+++ 209.minimum-size-subarray-sum.py
@@ -8,18 +8,17 @@
 # @lc code=start
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        if nums[0] >= target:
+            return 1
         min_length = float("inf")
-
         start, end, current_sum = 0, 0, 0
 
         # find the array that sums equal or greater than x first
         while end < len(nums):
+            if nums[end] >= target:
+                return 1
             current_sum = current_sum + nums[end]
             end = end + 1
-
-            if (end - start + 1) > min_length:
-                current_sum = current_sum - nums[start]
-                start = start + 1
 
             while start < end and current_sum >= target:
                 current_sum = current_sum - nums[start]
