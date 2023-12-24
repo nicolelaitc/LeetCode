5af79acc6f1f8678f0b525f09a33af10e2147ddc
yuiasora1024 <tcnlai@connect.ust.hk>
2023-12-24 14:36:17 -0500

    from forum

--- 71.simplify-path.py
+++ 71.simplify-path.py
@@ -8,18 +8,14 @@
 # @lc code=start
 class Solution:
     def simplifyPath(self, path: str) -> str:
+        path = path.split("/")
         stack = []
-        curr = ""
-        for c in path + "/":
-            if c == "/":
-                if curr == "..":
-                    if stack:
-                        stack.pop()
-                elif curr != "." and curr != "":
-                    stack.append(curr)
-                curr = ""
-            else:
-                curr += c
+        for p in path:
+            if p == "..":
+                if stack:
+                    stack.pop()
+            elif p not in ["", "."]:
+                stack.append(p)
         return "/" + "/".join(stack)
 
 
