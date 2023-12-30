df540d74914771f5e7fa23cdf060f43de6714465
yuiasora1024 <tcnlai@connect.ust.hk>
2023-12-30 16:52:11 -0500

    cleaner

--- 56.merge-intervals.py
+++ 56.merge-intervals.py
@@ -8,13 +8,12 @@
 # @lc code=start
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort()
+        intervals.sort(key=lambda x: x[0])
         i = 1
         while i < len(intervals):
-            start, end = intervals[i]
-            if start <= intervals[i - 1][1]:
+            if intervals[i][0] <= intervals[i - 1][1]:
                 intervals[i][0] = intervals[i - 1][0]
-                intervals[i][1] = max(intervals[i - 1][1], end)
+                intervals[i][1] = max(intervals[i - 1][1], intervals[i][1])
                 intervals.pop(i - 1)
             else:
                 i += 1
