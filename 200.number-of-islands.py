b7ed4f54e58b3480a8e043d62e099ef4710308ff
yuiasora1024 <tcnlai@connect.ust.hk>
2024-01-03 09:16:39 -0500

    cleaner

--- 200.number-of-islands.py
+++ 200.number-of-islands.py
@@ -8,7 +8,7 @@
 # @lc code=start
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
+        visited, stack = set(), list()
         island = 0
 
         for i in range(len(grid)):
@@ -16,28 +16,22 @@ class Solution:
                 if (i, j) in visited:
                     continue
 
-                if grid[i][j] == "1" and (i, j) not in visited:
+                if grid[i][j] == "1":
                     island += 1
-                    visited = self.check(grid, i, j, visited)
-
+                    stack.append((i, j))
+                    while stack:
+                        i, j = stack.pop()
+                        for x in range(i - 1, i + 2):
+                            for y in range(j - 1, j + 2):
+                                if x == i or y == j:
+                                    if 0 <= x < len(grid) and 0 <= y < len(grid[0]):
+                                        if (x, y) not in visited:
+                                            visited.add((x, y))
+                                            if grid[x][y] == "1":
+                                                stack.append((x, y))
                 visited.add((i, j))
 
         return island
-
-    def check(self, grid, i, j, visited):
-        stack = [(i, j)]
-        while stack:
-            i, j = stack.pop()
-            for x in range(i - 1, i + 2):
-                for y in range(j - 1, j + 2):
-                    if x == i or y == j:
-                        if 0 <= x < len(grid) and 0 <= y < len(grid[0]):
-                            if (x, y) not in visited:
-                                visited.add((x, y))
-                                if grid[x][y] == "1":
-                                    stack.append((x, y))
-
-        return visited
 
 
 # @lc code=end
